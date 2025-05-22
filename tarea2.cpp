f0548e8d5c1ce4ec4f9a8c75f2ac0be4947ab48b
Choripan <mibanezm@usm.cl>
2025-05-22 17:56:54 -0400

    Terminada la funcion leerEnemigos

--- tarea2.cpp
+++ tarea2.cpp
@@ -51,9 +51,43 @@ void leerHabitaciones(ifstream& archivo, Habitacion habitaciones[], int& numHabi
 
 
 
+struct Enemigo {
+    string nombre;
+    int vida, ataque;
+    float precision, probabilidad;
+};
 
+void leerEnemigos(ifstream& archivo, Enemigo enemigos[], int& numEnemigos) {
+    string linea; 
+    getline(archivo, linea);    //Lee la linea "ENEMIGOS"
+    if (linea != "ENEMIGOS"){
+        cout << "Error: No se encontro la seccion ENEMIGOS" << endl;
+        return;
+    }
 
+    getline(archivo, linea);     //Leer el numero de enemigos
+    numEnemigos = stoi(linea);   //Convierte la linea a int
 
+    //Leer cada enemigo
+    for (int i = 0; i < numEnemigos; i++) { // Ciclo que se repite para cada enemigo
+        getline(archivo, linea);  //Datos del enemigo
+        stringstream ss(linea); //Stringstream para convertir linea 
+        Enemigo enemigo; //Crear un objeto enemigo
+        getline(ss, enemigo.nombre, '|'); //Guarda el nombre dentro de la casilla del objeto
+        enemigo.nombre = enemigo.nombre.substr(0, enemigo.nombre.size()-1); //Quitar espacio al final en nombre
+        ss.ignore(6); //Ignorar " Vida "
+        ss >> enemigo.vida; //Guardar vida
+        ss.ignore(10); //Ignorar " | Ataque "
+        ss >> enemigo.ataque; //Guardar ataque
+        ss.ignore(13); //Ignorar " | Precision "
+        ss >> enemigo.precision; //Guardar precision
+        ss.ignore(15); //Ignorar " | Probabilidad "
+        ss >> enemigo.probabilidad; //Guardar probabilidad
+
+        enemigos[i] = enemigo; //Guardar enemigo en el arreglo enemigos
+
+    }
+}
 
 
 
@@ -85,6 +119,20 @@ int main() {
              << " (" << habitaciones[i].tipo << ")" << endl
              << "Descripcion: " << habitaciones[i].desc << endl;
     }
+
+    Enemigo enemigos[100];
+    int numEnemigos = 0; // Inicializar en 0
+    leerEnemigos(archivo, enemigos, numEnemigos);
+
+    // Imprimir enemigos
+    for (int i = 0; i < numEnemigos; i++) {
+        cout << "Enemigo " << i + 1 << ": " << enemigos[i].nombre
+             << " | Vida: " << enemigos[i].vida
+             << " | Ataque: " << enemigos[i].ataque
+             << " | Precision: " << enemigos[i].precision
+             << " | Probabilidad: " << enemigos[i].probabilidad << endl;
+    }
+
 
     archivo.close();
     return 0;
