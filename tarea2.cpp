18fb23b2a7e8560ae82401a83a163b352a71cbc8
whoskyzz <jgahona@usm.cl>
2025-05-21 20:03:32 -0400

    Creacion funcion LeerHabitaciones

--- tarea2.cpp
+++ tarea2.cpp
@@ -5,17 +5,87 @@
 #include <random>
 using namespace std;
 
+struct Habitacion {
+    // Estructura de habitacion
+    unsigned int id;
+    string name;
+    string desc;
+    string tipo;
+};
+
+void leerHabitaciones(ifstream& archivo, Habitacion habitaciones[], int& numHabitaciones) {
+    string linea; //Lee la linea "Habitaciones"
+    getline(archivo, linea);
+    if (linea != "HABITACIONES"){
+        cout << "Error: No se encontro la seccion HABITACIONES" << endl;
+        return;
+    }
+
+    getline(archivo, linea);     //Leer el numero de habitaciones
+    numHabitaciones = stoi(linea); // Convierte la linea a int
+
+    //Leer cada habitacion
+    for (int i = 0; i < numHabitaciones; i++) { // Leer la linea del id, name, desc y tipo.
+        getline(archivo, linea);
+
+        stringstream ss(linea); //stringstream para convertir linea 
+        unsigned int id;
+        string name, tipo;
+
+        ss >> id; //extrae id;
+        getline(ss, name, '('); //Leemos hasta el '(', extraemos name
+        name = name.substr(0, name.size()-1); // quitar espacio al final en name
+        getline(ss, tipo, ')'); // Leer hasta ')', extraer tipo
+        
+        //Almacenamos la lista
+        habitaciones[i].id = id;
+        habitaciones[i].name = name;
+        habitaciones[i].tipo = tipo;
+        
+        //extraer desc
+        string desc = "";
+        getline(archivo, desc);
+        habitaciones[i].desc = desc;
+    }
+}
+
+
+
+
+
+
+
+
+
 int main() {
     cout << "Juego Aventura - EDD Tarea 2" << endl;
-    cout << "Creado por: Jorge Gahona y Matias Ibañez " << endl;
+    cout << "Creado por: Jorge Gahona y Matias Ibañez" << endl;
 
     ifstream archivo("ejemplo.map");
     if (!archivo.is_open()) {
         cerr << "Error al abrir el Archivo" << endl;
         return 1;
     }
-    cout << "Archivo abierto correctamente." << endl;
+
+    string linea;
+    getline(archivo, linea); // Leer "INICIO DE ARCHIVO"
+    if (linea != "INICIO DE ARCHIVO") {
+        cerr << "Error: no se encontró INICIO DE ARCHIVO" << endl;
+        archivo.close();
+        return 1;
+    }
+
+    Habitacion habitaciones[100];
+    int numHabitaciones = 0; // Inicializar en 0
+    leerHabitaciones(archivo, habitaciones, numHabitaciones);
+
+    // Imprimir habitaciones
+    for (int i = 0; i < numHabitaciones; i++) {
+        cout << "Habitacion " << habitaciones[i].id << ": " << habitaciones[i].name
+             << " (" << habitaciones[i].tipo << ")" << endl
+             << "Descripcion: " << habitaciones[i].desc << endl;
+    }
+
     archivo.close();
     return 0;
-
 }
